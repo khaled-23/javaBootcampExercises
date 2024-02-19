11612ce4446b1d2cb1bbefe721a7d84e1ee19352
khaled-23 <121809084+khaled-23@users.noreply.github.com>
2024-02-19 11:23:44 +0300

    Add files via upload

---
+++ Exam1.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Exam1 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+//        //Q1
+//        System.out.println("enter first number");
+//        int num1 = s.nextInt();
+//        System.out.println("enter second number");
+//        int num2 = s.nextInt();
+//        System.out.println("enter third number");
+//        int num3 = s.nextInt();
+//
+//        boolean number1 = num1 > num2 && num1 > num3;
+//        boolean number2 = num2 > num1 && num2 > num3;
+//
+//        if(number1){
+//            System.out.println("first number is the largest \"" + num1 + "\"");
+//        }else if(number2){
+//            System.out.println("second number is the largest \"" + num2 + "\"");
+//        }else System.out.println("third number is the largest \"" + num3 + "\"");
+
+
+
+// -----------------------------------------------------------------------------------------------------
+
+
+//        //Q2
+//        System.out.println("enter a string");
+//        StringBuilder str = new StringBuilder(s.nextLine());
+//
+//        System.out.println("enter an index");
+//        int index = s.nextInt();
+//
+//        if(index<str.length()  && index>=0){
+//            System.out.println(str.charAt(index));
+//        }else System.out.println("index out of bound");
+
+
+// -----------------------------------------------------------------------------------------------------
+
+//            //Q3
+//            double sum = 0;
+//            double num = 0;
+//
+//            while(num!=-1){
+//                sum = sum + num;
+//                System.out.println("enter a number or write \"-1\" to exit");
+//                num = s.nextInt();
+//
+//            }
+//
+//        System.out.println(sum);
+
+
+// -----------------------------------------------------------------------------------------------------
+
+
+//        //Q4
+//        int[] numbers = {10, -21, 30, 31, -25};
+//
+//        for(int n:numbers){
+//            if(n<0){
+//                System.out.println(n + " is a negative number");
+//            }else System.out.println(n + " is a positive number");
+//        }
+
+
+
+// -----------------------------------------------------------------------------------------------------
+
+//        //Q5
+//        String[] names = {"Tuwaiq", "Bootcamp", "Student", "JAVA"};
+//
+//        String shortest = names[0];
+//        String temp = names[0];
+//        for(String n: names){
+//
+//            if(temp.length()>n.length()){
+//                shortest = n;
+//            }
+//                temp = n;
+//        }
+//        System.out.println(shortest);
+
+
+    }
+}
